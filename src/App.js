794474cf8b33e68714aa312d2c6e2d282191e348
dolfant <louis.cascio@hotmail.fr>
2019-10-22 10:14:23 +0200

    commentaires sur le code de la structure Home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+// import des différents composants
 import Connect from './components/Connect';
 import Weather from './components/Weather';
 import SearchBar from './components/SearchBar';
@@ -11,16 +13,27 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">
+
+        {/* module de connexion sign in/up */}
         <Connect />
+
+        {/* affichage météo relié à une API */}
         <Weather />
+
+        {/* bare de recherche lié à une API plante */}
         <SearchBar />
+
+        {/* grille suggestion plantes */}
         <PlantList />
+
+        {/* infos / réseaux sociaux */}
         <Footer />
+
+        {/* menu de l'appli une fois connecté garden/board/alerts */}
         <NavBar />
       </div>
     );
   }
 }
-
 
 export default App;
