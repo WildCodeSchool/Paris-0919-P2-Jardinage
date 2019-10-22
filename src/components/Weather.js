adb8cec60b5317d06df1f822781c5661fd83dfcd
Valentine Voyen <valentine.voyen1@gmail.com>
2019-10-22 14:35:07 +0200

    first try

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,43 @@
 import React from 'react';
-import '../App.scss';
 
-function Weather() {
-    return (
-        <div id= "weather">
-        </div>
-    );
+const API_KEY = "5fb2333ba75358bc5030738944a75305"; 
+class Weather extends React.Component {
+state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
+    const data = await api_call.json();
+    if (city && country) {
+    console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ''
+    });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the value."
+      });
+    }
+  }
 }
 
 export default Weather;
