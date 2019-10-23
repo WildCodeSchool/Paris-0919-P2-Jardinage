29c7099fa7378440ad1e6fdcc069efd1610d5036
Valentine Voyen <valentine.voyen1@gmail.com>
2019-10-23 09:12:57 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,26 @@ import Footer from './components/Footer.js';
 import NavBar from './components/NavBar.js';
 import './App.scss';
 
+const API_KEY = "5fb2333ba75358bc5030738944a75305"; 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div className="app">
@@ -18,7 +37,14 @@ class App extends React.Component {
         <Connect />
 
         {/* affichage météo relié à une API */}
-        <Weather />
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
 
         {/* bare de recherche lié à une API plante */}
         <SearchBar />
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import '../App.scss';
+import React from 'react'
 
-function Weather() {
-    return (
-        <div id= "weather">
-        </div>
-    );
-}
+const Weather = props => (
+  <div id='weather'>
+    <p>Location: {props.city}, {props.country}</p>
+    <p>Temperature: {props.temperature}</p>
+    <p>Humidity: {props.humidity}</p>
+    <p>Conditions: {props.description}</p>
+    <p>{props.error}</p>
+  </div>
+)
+
 
 export default Weather;
