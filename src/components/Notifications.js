4975247c2f06859396d7751e70940a15cb6bc372
Mboulgour <mathieu.r.77@gmail.com>
2019-10-28 17:41:38 +0100

    FontA icons added + Notifications (mock data) DONE -> cleaning

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
+import Notification from './Notification'
 
 import './style/Notifications.scss'
 
 const sampleNotifs = [
   {
-    date: "24/10/2019",
+    date: '28/10/2019',
     notifs:[
       {
         type: "checking",
@@ -16,11 +17,16 @@ const sampleNotifs = [
         type: "weeding",
         plant_id: 1,
         checked: false,
+      },
+      {
+        type: "watering",
+        plant_id: 5,
+        checked: false,
       }
     ]
   },
   {
-    date:"28/10/2019",
+    date:"24/10/2019",
     notifs:[
       {
         type: "checking",
@@ -39,22 +45,27 @@ const sampleNotifs = [
 class Notifications extends React.Component {
   render(){
     return(
+
       <div className="Notifications-container">
         <h1>Notifications</h1>
-        {sampleNotifs.map(elm =>{
+        {sampleNotifs.map(elm =>
+          {
             return(
               <div className="Notification">
                 <div className="Notification-date">
                   {elm.date}
                 </div>
-              {elm.notifs.map(notif =>{
-                return <p>{notif.type}</p>
-              })}
+                {elm.notifs.map(notif =>
+                  {
+                    return <Notification notif={notif} />
+                  })
+                }
               </div>
             )
           })
         }
       </div>
+
     );
   };
 };
