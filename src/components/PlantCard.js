68419448119e0f8ec266020d1e7967f922a8eb20
Claire <clairepelletier.prod@gmail.com>
2019-10-31 00:14:58 +0100

    change display API res

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -5,10 +5,10 @@ import './style/PlantCard.scss'
 const PlantCard = props => (
   <figure className="plantCard">
     {props.image && props.common_name && <img className="plantCard-img" src={props.image} alt={props.common_name} />} 
+    <img className="plantCard-icon" src={iconAdd} alt="icon add" />
     <figcaption className={props.visible_caption ? "subtitle" : "invisible"}>
       {props.common_name && <strong>{props.common_name}</strong>} 
       {props.scientific_name && <em>{props.scientific_name}</em>}
-      <img className="plantCard-icon" src={iconAdd} alt="icon add" />
     </figcaption>
   </figure>
 )
