b7c57ea3e9e3d4a69c06f3bb7757c8e54d4fded5
Claire <clairepelletier.prod@gmail.com>
2019-10-30 23:05:18 +0100

    modified default img

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,10 +10,6 @@ import './style/PlantCard.scss'
 const API_KEY = "YjlIUlp5QktVcXRIZTEzVGNMSmlOZz09"
 let finalResult = []
 
-const getRandomInt = max => {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
 class Search extends React.Component {
   state = { 
     id: undefined,
@@ -25,26 +21,30 @@ class Search extends React.Component {
     isLoaded: false
   }
 
+  getRandomInt = max => {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
   getPlant = async e => {
     finalResult = []
     e.preventDefault()
     const common_name = e.target.common_name.value
-    const default_img = "https://s2.best-wallpaper.net/wallpaper/1600x900/1708/Art-drawing-tree-earth-green_1600x900.jpg"
+    const default_img = "https://res.cloudinary.com/dsbgj0oop/image/upload/v1572470792/default_img.png"
     const api_1st_call = await fetch(`https://trefle.io/api/plants?q=${common_name}&complete_data=true&token=${API_KEY}`)
     const data = await api_1st_call.json()
-    const idTen = data.filter((item, index) => index < 11)
+    // const idTen = data.filter((item, index) => index < 11)
     
-    if(idTen[0]){
-      for (let i=0; i < idTen.length; i++) {
-        const api_2nd_call = await fetch(`https://trefle.io/api/plants/${idTen[i].id}?token=${API_KEY}`)
+    if(data[0]){
+      for (let i=0; i < data.length; i++) {
+        const api_2nd_call = await fetch(`https://trefle.io/api/plants/${data[i].id}?token=${API_KEY}`)
         const data_specific = await api_2nd_call.json()
         const species = data_specific.main_species ? data_specific.main_species.common_name:'undefined'
-        if(common_name && idTen[0]){
+        if(common_name && data[0]){
           this.setState({
             id: data_specific.id,
             common_name: species,
             scientific_name: data_specific.scientific_name,
-            image: data_specific.images[0] !== undefined ? data_specific.images[getRandomInt(data_specific.images.length)].url : default_img,
+            image: data_specific.images[0] !== undefined ? data_specific.images[this.getRandomInt(data_specific.images.length)].url : default_img,
             visible_caption: true,
             isLoaded: true,
             error: undefined
