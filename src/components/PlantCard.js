599b93c2996e8739d237813ceb99090d75cd21ca
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-11-05 16:50:27 +0100

    save ids to localstorage

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,34 +1,49 @@
-import React from 'react'
-import iconAdd from '../icon-plus.svg'
-import './style/PlantCard.scss'
+import React from 'react';
+import iconAdd from '../icon-plus.svg';
+import './style/PlantCard.scss';
 
 class PlantCard extends React.Component {
-
   componentDidMount() {
-    localStorage.setItem("ids", JSON.stringify([]))
-    
+    if (localStorage.getItem('ids') === null) {
+      //...
+      localStorage.setItem('ids', JSON.stringify([]));
+    }
   }
 
-  getId = () => {
-    let numId = this.props.id
+  addIdToLocalStorge = e => {
+    const ids = JSON.parse(localStorage.getItem('ids'));
+    ids.push(this.props.id);
 
-
-    localStorage.setItem('id', numId)
-    console.log(numId)
-  }
+    localStorage.setItem('ids', JSON.stringify(ids));
+  };
 
   render() {
     return (
-      <figure className={this.props.oneItemResult ? "plantCard lonely":"plantCard"}>
-        {this.props.image && this.props.common_name && <img className="plantCard-img" src={this.props.image} alt={this.props.common_name} />} 
-        <img className="plantCard-icon" src={iconAdd} alt="icon add" onClick={this.getId} />
-        <figcaption className={this.props.visible_caption ? "subtitle" : "invisible"}>
-          {this.props.common_name && <strong>{this.props.common_name}</strong>} 
+      <figure
+        className={this.props.oneItemResult ? 'plantCard lonely' : 'plantCard'}
+      >
+        {this.props.image && this.props.common_name && (
+          <img
+            className="plantCard-img"
+            src={this.props.image}
+            alt={this.props.common_name}
+          />
+        )}
+        <img
+          className="plantCard-icon"
+          src={iconAdd}
+          alt="icon add"
+          onClick={this.addIdToLocalStorge}
+        />
+        <figcaption
+          className={this.props.visible_caption ? 'subtitle' : 'invisible'}
+        >
+          {this.props.common_name && <strong>{this.props.common_name}</strong>}
           {this.props.scientific_name && <em>{this.props.scientific_name}</em>}
         </figcaption>
       </figure>
-    )
+    );
   }
 }
-    
-export default PlantCard
+
+export default PlantCard;
