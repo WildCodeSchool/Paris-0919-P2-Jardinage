cd8148a4b9f8be5e4ce7dedcd86f2bbcab2a2fea
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-11-06 20:11:02 +0100

    fix style

--- src/components/Garden.js
+++ src/components/Garden.js
@@ -4,30 +4,35 @@ import React from 'react';
 import Footer from './Footer.js';
 import NavBar from './NavBar.js';
 import Search from './Search';
-import GardenList from './gardenComp/GardenList'
-import NavMobile from './NavMobile'
+import GardenList from './gardenComp/GardenList';
+import NavMobile from './NavMobile';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheckCircle, faTint, faSeedling, faLeaf  } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCheckCircle,
+  faTint,
+  faSeedling,
+  faLeaf
+} from '@fortawesome/free-solid-svg-icons';
 
 import '../App.scss';
-import './style/searchBar.scss'
+import './style/searchBar.scss';
 
 class Garden extends React.Component {
   state = {
     isOnline: false,
     email: ''
-  }
+  };
   componentDidMount() {
     const email = localStorage.getItem('email');
     if (email) {
       this.setState({
         isOnline: true
-      })
+      });
     } else {
       this.setState({
         isOnline: false
-      })
+      });
     }
   }
 
@@ -35,26 +40,27 @@ class Garden extends React.Component {
     if (localStorage.id === undefined) {
       return (
         <div>
-          <p className="plantCard-error">Your garden is empty! You can add plants to take care of... :) <FontAwesomeIcon icon={faLeaf} /></p>
+          <p className="plantCard-error">
+            Your garden is empty! You can add plants to take care of... :){' '}
+            <FontAwesomeIcon icon={faLeaf} />
+          </p>
         </div>
-      )
+      );
     }
-  }
+  };
 
   render() {
-    const isThereAPlant = localStorage.ids
-    
+    const isThereAPlant = localStorage.ids;
+
     return (
       <div className="app">
-
         {/* module de connexion sign in/up */}
         <NavBar />
 
         {/* bare de recherche lié à une API plante */}
         <Search />
-        <div>
-          {isThereAPlant ? <GardenList /> : this.gardenInfo()}
-        </div>
+
+        {isThereAPlant ? <GardenList /> : this.gardenInfo()}
 
         {/* grille suggestion plantes
         <GardenList /> */}
@@ -66,9 +72,8 @@ class Garden extends React.Component {
         <Footer />
 
         {/* menu de l'appli une fois connecté garden/board/alerts */}
-
       </div>
-    )
+    );
   }
 }
 
