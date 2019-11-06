77011f1f0f5e68a3eec183e58b9c6b9f8a1b4a32
Claire <clairepelletier.prod@gmail.com>
2019-11-06 09:37:38 +0100

    merge dev + add conditional display message on number of plants found

--- src/components/Search.js
+++ src/components/Search.js
@@ -18,6 +18,7 @@ class Search extends React.Component {
     scrollMsg: false
   }
 
+  // Fonction random qui sera utilisée pour afficher aléatoirement les images disponibles
   getRandomInt = max => {
     return Math.floor(Math.random() * Math.floor(max));
   }
@@ -27,18 +28,24 @@ class Search extends React.Component {
     e.preventDefault()
     const common_name = e.target.common_name.value
     const default_img = "https://res.cloudinary.com/dsbgj0oop/image/upload/v1572516426/default_img.png"
+    // Premier appel API
     const api_1st_call = await fetch(`https://trefle.io/api/plants?q=${common_name}&complete_data=true&token=${API_KEY}`)
     const data = await api_1st_call.json()
     
+    // Si le premier appel API renvoie des résultats...
     if(data[0]){
+      // ... et que l'utilisateur a bien tapé quelque chose dans l'input
       if(common_name && data[0]){
+        // ...on lance un second appel API en boucle sur chaque ID de plante récupérée dans le 1er appel API 
         for (let i=0; i < data.length; i++) {
           const api_2nd_call = await fetch(`https://trefle.io/api/plants/${data[i].id}?token=${API_KEY}`)
           const data_plant = await api_2nd_call.json()
           const species = data_plant.main_species ? data_plant.main_species.common_name:'undefined'
           const randImage = data_plant.images[0] !== undefined ? data_plant.images[this.getRandomInt(data_plant.images.length)].url : default_img
           finalResult.push({id: data_plant.id, common_name: species, scientific_name:data_plant.scientific_name, image: randImage})
-        } if (finalResult.length > 1) {
+        } 
+        // Condition d'affichage selon le nombre de plantes à afficher
+        if (finalResult.length > 1) {
           this.setState({
             visible_caption: true,
             oneItemResult: false,
