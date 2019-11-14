a8ebc884c86319a77ddb97db6edec302a55d29d9
Nicolas Le Pasteur <nicolas.lepasteur@gmail.com>
2019-11-14 10:35:03 +0100

    style garden button display

--- src/components/gardenComp/GardenList.js
+++ src/components/gardenComp/GardenList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrashAlt  } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 import '../style/GardenList.scss'
 
@@ -23,7 +23,7 @@ class GardenList extends React.Component {
               <h3><em>{obj.scientific_name}</em></h3>
             </div>
             <FontAwesomeIcon className="fa-trash-alt" icon={faTrashAlt}
-              onClick={()=>this.props.handleDeletePlant(index)}
+              onClick={() => this.props.handleDeletePlant(index)}
             />
           </figure>
         ))
@@ -37,7 +37,7 @@ class GardenList extends React.Component {
               <h3>{obj.common_name}</h3>
               <h3><em>{obj.scientific_name}</em></h3>
               <FontAwesomeIcon className="fa-trash-alt" icon={faTrashAlt}
-                onClick={()=>this.props.handleDeletePlant(index)}
+                onClick={() => this.props.handleDeletePlant(index)}
               />
             </div>
           </figure>
@@ -55,7 +55,7 @@ class GardenList extends React.Component {
     return (
       <div className='bigWrapper'>
 
-        <button onClick={this.ChangeDisplayGarden}>{this.state.displayGarden === 'grid' ? 'list' : 'grid'}</button>
+        <button onClick={this.ChangeDisplayGarden} className='garden_button'>{this.state.displayGarden === 'grid' ? 'List' : 'Grid'}</button>
 
         <div className={this.state.displayGarden}>
           {this.props.plantsAdded.length > 0 && this.theRender()}
